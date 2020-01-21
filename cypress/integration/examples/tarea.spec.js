a528c192f2b7db2cb1aa738f802809c8b41cd973
andersongenez <unitymaxx1@gmail.com>
2020-01-21 16:25:14 -0300

    Tarea hecha

--- cypress/integration/examples/tarea.spec.js
+++ cypress/integration/examples/tarea.spec.js
@@ -2,7 +2,8 @@
 describe('Funcionalidad quejas', function() {
   it('Burla al usuario', function() {
     cy.visit('http://automationpractice.com/')
-    cy.contains("Faded")
+    cy.get('#search_query_top').type("Faded")
+    cy.get('#search_query_top').type("{enter}")
     cy.contains("Add to cart").click()
     cy.contains("Proceed to checkout").click()
     cy.get("#cart_quantity_up_1_1_0_0").click()
